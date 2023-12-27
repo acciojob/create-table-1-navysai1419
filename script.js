6beab8d6ab30aa6fffd52ddb41751524674fc14b
Navya <88023981+navysai1419@users.noreply.github.com>
2023-12-27 15:36:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function insert_Row() {
     //Write your code here
+	var table = document.getElementById("sampleTable");
+    var newRow = table.insertRow(1); // Inserts a new row at index 1 (just after the header row)
+
+    var cell1 = newRow.insertCell(0); // Inserts a cell at index 0
+    var cell2 = newRow.insertCell(1); // Inserts a cell at index 1
+
+    // You can set the content of the cells here, for example:
+    cell1.innerHTML = "New Name";
+    cell2.innerHTML = "newemail@example.com";
   
   
 }
